3ec202835d441551dcff135385760102870b91e3
Cameron Whitehead <camjw119@gmail.com>
2018-09-10 14:14:00 +0100

    Added top_up and exceeds_maximum?

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,7 +1,21 @@
 class OysterCard
-  attr_reader :balance
+  DEFAULT_MAXIMUM = 90
+  attr_reader :balance, :maximum_balance
 
-  def initialize
+  def initialize(maximum_balance = DEFAULT_MAXIMUM)
     @balance = 0
+    @maximum_balance = maximum_balance
   end
+
+  def top_up(value)
+    fail "Exceeded maximum card balance is #{maximum_balance}" if exceeds_maximum?(value)
+    @balance += value
+  end
+
+  # private
+
+  def exceeds_maximum?(value)
+    balance + value > maximum_balance
+  end
+
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -4,4 +4,20 @@ describe OysterCard do
   it 'should respond to balance queries' do
     expect(subject.balance).to eq 0
   end
+
+  it 'should be able to change the maximum balance' do
+
+  end
+
+  context "when the user calls top_up" do
+    it 'should check that the balance increases by the right amount' do
+      initial_balance = subject.balance
+      expect(subject.top_up(50)).to eq initial_balance + 50
+    end
+
+    it 'fails if the maximum balance is exceeded' do
+      message = "Exceeded maximum card balance"
+      expect { subject.top_up(subject.maximum_balance + 1) }.to raise_error message
+    end
+  end
 end
