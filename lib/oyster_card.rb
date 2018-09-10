694a28b7f910f3d8dab28cc80cfaf032173b5f78
NadiaAiraf <aidanfaria@Aidans-MacBook-Pro.local>
2018-09-10 14:30:18 +0100

    Challenge 9, adding the deduct money, touch in and touch out functions and variable dictating if a card is in journey or not

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -5,11 +5,24 @@ class OysterCard
   def initialize(maximum_balance = DEFAULT_MAXIMUM)
     @balance = 0
     @maximum_balance = maximum_balance
+    @in_journey = false
   end
 
   def top_up(value)
-    fail "Exceeded maximum card balance is #{maximum_balance}" if exceeds_maximum?(value)
+    fail "Exceeded maximum card balance. Maximum is #{maximum_balance}" if exceeds_maximum?(value)
     @balance += value
+  end
+
+  def deduct(value)
+    @balance -= value
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
   # private
@@ -18,4 +31,7 @@ class OysterCard
     balance + value > maximum_balance
   end
 
+  def in_journey?
+    @in_journey
+  end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -16,8 +16,30 @@ describe OysterCard do
     end
 
     it 'fails if the maximum balance is exceeded' do
-      message = "Exceeded maximum card balance"
+      message = "Exceeded maximum card balance. Maximum is #{subject.maximum_balance}"
       expect { subject.top_up(subject.maximum_balance + 1) }.to raise_error message
+    end
+  end
+
+  context "when the users calls deduct" do
+    it 'should remove the correct amount from the balance' do
+      initial_balance = subject.balance
+      expect(subject.deduct(50)).to eq initial_balance - 50
+    end
+  end
+
+  context "when the users touches in" do
+    it 'should make the in_journey variable true' do
+      subject.touch_in
+      expect(subject.in_journey?).to eq true
+    end
+  end
+
+  context "when a user is already touched in" do
+    it 'the touch_out method should set in_journey to false' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey?).to eq false
     end
   end
 end
